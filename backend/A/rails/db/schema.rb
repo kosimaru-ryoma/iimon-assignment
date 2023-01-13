038e44d064be19bf3a1b3e5cbcc5af67a0427be1
kosimaru-ryoma <koshimaru1997128@gmail.com>
2023-01-14 05:58:32 +0900

    create item table migration file

--- backend/A/rails/db/schema.rb
+++ backend/A/rails/db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_13_204327) do
+  create_table "items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", limit: 30, null: false
+    t.integer "price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_items_on_name", unique: true
+  end
+
 end
