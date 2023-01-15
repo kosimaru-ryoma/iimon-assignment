1d080b561a6610a3ade0c4fc6a861a9f4b7c6932
kosimaru-ryoma <122638083+kosimaru-ryoma@users.noreply.github.com>
2023-01-16 00:03:42 +0900

    Revert "ログイン機能を実装"

--- backend/B/rails/config/routes.rb
+++ backend/B/rails/config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
 
-  root   'static_pages#home'
-  get    '/signup', to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  resources :users, only: [:show, :create]
-
+  root "static_pages#home"
+  
 end
