e781bb1c7be94377d1d90f3b0b55b34092b91727
kosimaru-ryoma <koshimaru1997128@gmail.com>
2023-01-29 05:03:52 +0900

    Store情報を用いたviewの仮実装

--- frontend/C/frontend-C/src/App.tsx
+++ frontend/C/frontend-C/src/App.tsx
@@ -1,8 +1,32 @@
 import './App.css';
-import React from 'react';
+import { useAppDispatch, useAppSelector } from './redux/hooks';
+import { moveRight, moveLeft } from './redux/wordListSlice';
 
 const App = () => {
-  return <div className="App"></div>;
+
+  const leftSideWordList = useAppSelector(
+    (state) => state.wordList.leftSideWordList,
+  );
+  const rightSideWordList = useAppSelector(
+    (state) => state.wordList.rightSideWordList,
+  );
+  const dispatch = useAppDispatch();
+  return (
+    <div className="App">
+      <ul>
+        {leftSideWordList.map((v, i) => {
+          return <li key={i}>{v}</li>;
+        })}
+        <button onClick={() =>  dispatch(moveRight())}>右へ</button>
+      </ul>
+      <ul>
+        {rightSideWordList.map((v, i) => {
+          return <li key={i}>{v}</li>;
+        })}
+        <button onClick={() => dispatch(moveLeft())}>左へ</button>
+      </ul>
+    </div>
+  );
 };
 
 export default App;
